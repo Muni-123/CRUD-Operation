0646847c3bae511b7581fbd64eccf65baec3f2ff
Muni-123 <129214799+Muni-123@users.noreply.github.com>
2023-03-28 23:41:40 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// /***Prajwal Sir Crud op * */
+import React from 'react'
+import Homepage from './Components/Homepage'
+import Createusers from './Components/Createusers'
+import Users from './Components/Users'
+import Editusers from './Components/Editusers'
+import Deleteusers from './Components/Deleteusers'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+const App = () => {
+return (
+<div>
+<BrowserRouter>
+<Homepage/>
+<Routes>
+<Route path="/" element={<Createusers/>}></Route>
+<Route path="/edit/:index" element={<Editusers/>}></Route>
+<Route path="/delete/:index" element={<Deleteusers/>}></Route>
+<Route path="/users" element={<Users/>}></Route></Routes></BrowserRouter>
+</div>
+)
+}
+export default App
---
+++ src/Components/Deleteusers.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const Deleteusers = () => {
+return (
+<div>Deleteusers</div>
+)
+}
+export default Deleteusers
